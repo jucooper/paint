82d00853e7e14bec56f60947335c25973395686a
Justin Cooper <cooperjustin92@yahoo.com>
2014-11-07 08:56:43 -0500

    Added author

--- PaintProgram/src/simplePaint.java
+++ PaintProgram/src/simplePaint.java
@@ -1,9 +1,11 @@
-/* Author: 814930756
- * Last Modified: 10/31/14
+/* Author: Justin Cooper
  * This is a simple paint program that can draw 3 shapes in four colors. Rectangle, Oval, and Line. Red, Green, Blue and Black
  * The User can also decide if the Shape is to be filled with the color or not. 
  * The user may undo the action or clear the entire paint. As well as move the recently drawn shapes.
+ * Last Modified: 10/31/14
  */
+
+
 
 import java.awt.*;
 import java.awt.event.*;
